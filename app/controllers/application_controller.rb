ca03a5b1bbc0790744d382ef79c2344ae67e5f40
+ <+>
2022-11-27 21:54:44 +0300

    Assignment completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+
+# GET /birds/:id
+def show
+    # params[:id] refers to the dynamic part of our route, defined by :id
+    # a request to /birds/2 would give params[:id] a value of 2
+    bird = Bird.find_by(id: params[:id])
+    render json: bird
+  end
+
+#POST /birds
+def Create
+    bird = Bird.create(name: params[:name], species: params[:species])
+    render json: bird, status: :created
+end
 end
